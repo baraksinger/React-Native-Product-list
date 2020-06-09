116fab183220b912a697a8474ed58b27d7ceb5a5
Barak <baraksinger20@gmail.com>
2020-06-09 21:32:40 +0300

    added search functionality

--- App.js
+++ App.js
@@ -5,9 +5,21 @@ const productsData = require('./dataProvider.json');
 export default function App() {
   const [searchText, setSearchText] = useState('');
   const [products, setProducts] = useState([...productsData]);
+  const [filteredProducts, setFilteredProducts] = useState([...productsData]);
 
   const searchTextHandler = (text) => {
     setSearchText(text)
+  }
+
+  const searchSubmitHandler = (event) => {
+    let filter = event.nativeEvent.text.toLowerCase();
+    let filteredResults = [];
+    for (let product of products) {
+      if (product.name.toLowerCase().indexOf(filter) > -1) {
+        filteredResults.push(product);
+      }
+    }
+    setFilteredProducts(filteredResults);
   }
 
   return (
@@ -18,11 +30,12 @@ export default function App() {
             placeholder="Search Products..."
             onChangeText={searchTextHandler}
             value={searchText}
+            onSubmitEditing={searchSubmitHandler}
         />
       </View>
       <FlatList
           keyExtractor={(item) => item.id}
-          data={products}
+          data={filteredProducts}
           renderItem={ItemData => (
               <View style={styles.listItem}>
                 <Text>{ItemData.item.name}</Text>
